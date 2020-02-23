46388e507fa33361ef705156328c9bebbbe32e58
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2020-02-23 18:13:12 +0200

    feat: add solution for typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+
+    array.sort(function(a, b) {
+      return a - b;
+    });
+
+    return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+
+    array.sort(function(a, b) {
+      return a - b;
+    });
+
+    return array[array.length - 1]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  
+  return sum/array.length;
 }
